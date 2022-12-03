e9f30906e777f5f79c6dc170adf0d2f3615f1851
Solene49000 <103738219+Solene49000@users.noreply.github.com>
2022-12-03 14:00:41 +0100

    Update runlib.h

--- Codes/runlib.h
+++ Codes/runlib.h
@@ -1,8 +1,12 @@
+
 #include <iostream>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_color.h>
+#include <allegro5/allegro_primitives.h>
 #include "variables.h"
 
-void rungame(void);
+void rungame(ALLEGRO_DISPLAY * display);
+void is_mouse_in_tour(int x, int y, bool *isintour1, bool *isintour2, bool *isintour3);
+void display_tours();
