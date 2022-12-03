a6b0604f1a1761b8e2b7e3ef13b9464b4ba3479e
Solene49000 <103738219+Solene49000@users.noreply.github.com>
2022-12-03 13:59:14 +0100

    Update rungame.cpp

--- Codes/rungame.cpp
+++ Codes/rungame.cpp
@@ -1,75 +1,109 @@
 //PROJET TOURS HANOI - - ALLOUIS GUILLAUME
-#include <iostream>
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_ttf.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_color.h>
 #include "runlib.h"
 #include "variables.h"
 
-//VARIABLES GLOBALES A SUPP DES QUE LIBS TOURS ET ANNEAUX FAITS
+//DEMMARAGE
+void rungame(ALLEGRO_DISPLAY * display) {
+	//Initialisation librairies
+	al_init_primitives_addon();
+	//Initialisation gestionnaire des events
+	ALLEGRO_EVENT_QUEUE * event_queue = al_create_event_queue();
+	al_register_event_source(event_queue, al_get_display_event_source(display));
+	al_register_event_source(event_queue, al_get_mouse_event_source());
+	//Initialisation des variables
+	int x = 0, y = 0;
+	bool loop = true;
+	bool * isintour1 = (bool*)malloc(sizeof(bool));
+	bool * isintour2 = (bool*)malloc(sizeof(bool));
+	bool * isintour3 = (bool*)malloc(sizeof(bool));
+	*isintour1=false;
+	*isintour2=false;
+	*isintour3=false;
+	bool selection = false; //Selection prend la valeur TRUE si un disque est selectionné
+	
+	//Gestionnaire des evenements
+	al_create_event_queue();
+	    while (loop){
+		display_tours();
+		ALLEGRO_EVENT events;
+		al_wait_for_event(event_queue, &events);
+		//Si appui sur croix rouge : fermer fenêtre
+		if(events.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
+	            loop=false;
+	        }
 
-void rungame(void) {
+	        // si mouvement de souris : enregistrer la position
+	        if (events.type == ALLEGRO_EVENT_MOUSE_AXES ){
+	            x=events.mouse.x;
+	            y=events.mouse.y;
+	        }
 
-ALLEGRO_MOUSE_STATE mouse; //Souris simple
-int oldx = 0, oldy = 0;
-bool fin = false, isintour1 = false, isintour2 = false, isintour3 = false;
-int tour1posx=100;
-int tour2posx=320;
-int tour3posx=440;
-int tourposy=80;
-int tourlargeur=100;
-int tourhauteur=300;
-bool selection = false; //Selection prend la valeur TRUE si un disque est selectionné
+		is_mouse_in_tour(x,y,isintour1,isintour2,isintour3);
 
-    while (!fin){
+		//si clic sur tour pour selectionner un disque : Appel fonction supprimer disque
+		if (events.type==ALLEGRO_EVENT_MOUSE_BUTTON_DOWN && selection==false && (*isintour1 == true || *isintour2 == true || *isintour3 == true))
+		{
+			if(events.mouse.button & 1)
+			{
+				selection = false;
+				al_clear_to_color(al_map_rgb(rand() % 256,rand() % 256, rand() % 256));
+			//Appel prog pour supprimer le disque de la tour concernée
+			}
+		}
+		//si clic sur tour pour poser disque : Appel fonction pose disque
+		if(events.type==ALLEGRO_EVENT_MOUSE_BUTTON_DOWN && selection==true && (*isintour1 == true || *isintour2 == true || *isintour3 == true))
+		{
+			if(events.mouse.button & 1)
+			{
+				selection = false;
+				al_clear_to_color(al_map_rgb(0,0,0));
+			}
+		}
+		//Afficher nouvel écran
+	        al_flip_display();
+	}//Fin gestionnaire des evenements
+	//Destructeur
+	al_destroy_event_queue(event_queue);
+}
 
-        // récupération de l'état de la souris
-        al_get_mouse_state(&mouse);
-
-        // si mouvement enregistrer la position
-        if (oldx != mouse.x || oldy != mouse.y){
-            oldx = mouse.x;
-            oldy = mouse.y;
-            //printf("%d-%d\n", mouse.x, mouse.y);
-        }
-
+void is_mouse_in_tour(int x, int y, bool *isintour1, bool *isintour2, bool *isintour3 ){
+//Variables position tour (A METTRE EN VAR GLOBALES)
+	int tour1posx=100;
+	int tour2posx=320;
+	int tour3posx=440;
+	int tourposy=80;
+	int tourlargeur=100;
+	int tourhauteur=300;
 	//Position dans une tour ?
-	if(tour1posx < oldx && oldx < (tour1posx + tourlargeur) && tourposy < oldy && oldy < (tourposy + tourhauteur))
+	if(tour1posx < x && x < (tour1posx + tourlargeur) && tourposy < y && y < (tourposy + tourhauteur))
 	{
-		isintour1 = true;
+		*isintour1 = true;
 	}
-	else if(tour2posx < oldx && oldx < (tour2posx + tourlargeur) && tourposy < oldy && oldy < (tourposy + tourhauteur))
+	else if(tour2posx < x && x < (tour2posx + tourlargeur) && tourposy < y && y < (tourposy + tourhauteur))
 	{
-		isintour2 = true;
+		*isintour2 = true;
 	}
-	else if(tour3posx < oldx && oldx < (tour3posx + tourlargeur) && tourposy < oldy && oldy < (tourposy + tourhauteur))
+	else if(tour3posx < x && x < (tour3posx + tourlargeur) && tourposy < y && y < (tourposy + tourhauteur))
 	{
-		isintour3 = true;
+		*isintour3 = true;
 	}
 	else{
-		isintour1 = false;
-		isintour2=false;
-		isintour3=false;
+		*isintour1 = false;
+		*isintour2=false;
+		*isintour3=false;
 	}
-	//si clic sur tour pour selectionner un disque
-	if (mouse.buttons & 1 && selection==false && (isintour1 == true || isintour2 == true || isintour3 == true))
-	{
-		selection = false;
-		al_clear_to_color(al_map_rgb(rand() % 256,rand() % 256, rand() % 256));
-		//Appel prog pour supprimer le disque de la tour concernée
-	}
-	//si clic sur tour pour poser disque
-	if(mouse.buttons & 1 && selection==true && (isintour1 == true || isintour2 == true || isintour3 == true))
-	{
-		selection = false;
-		al_clear_to_color(al_map_rgb(0,0,0));
-	}
-	// si clic droit quitter
-        if (mouse.buttons & 2)
-            fin = true;
+}
+void display_tours(){
 
-        al_flip_display();
-    }
+	int tour1posx=100;
+	int tour2posx=320;
+	int tour3posx=440;
+	int tourposy=80;
+	int tourlargeur=100;
+	int tourhauteur=300;
 
+	//Rectangles représentant les tours
+	al_draw_filled_rectangle(tour1posx, tourposy, tour1posx+tourlargeur, tourposy+tourhauteur,al_map_rgb(0,0,0));
+	al_draw_filled_rectangle(tour2posx, tourposy, tour2posx+tourlargeur, tourposy+tourhauteur,al_map_rgb(0,0,0));
+	al_draw_filled_rectangle(tour3posx, tourposy, tour3posx+tourlargeur, tourposy+tourhauteur,al_map_rgb(0,0,0));
 }
