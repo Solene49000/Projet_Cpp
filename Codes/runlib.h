b0099474418e0c3f9e1ee601434e74b54675d671
Solene49000 <103738219+Solene49000@users.noreply.github.com>
2022-12-04 02:19:19 +0100

    Update runlib.h

--- Codes/runlib.h
+++ Codes/runlib.h
@@ -7,6 +7,61 @@
 #include <allegro5/allegro_primitives.h>
 #include "variables.h"
 
-void rungame(ALLEGRO_DISPLAY * display);
+//Structures
+class Rectangle
+{
+	public:
+		int posx;
+		int posy;
+		ALLEGRO_COLOR color;
+	//ADD CONSTRUCTOR
+	Rectangle(int position_x,int position_y, ALLEGRO_COLOR couleur){
+		posx = position_x;
+		posy = position_y;
+		color = couleur;
+	}
+};
+
+class GrandRectangle : public Rectangle
+{
+	public:
+		int largeur = 400;
+		int hauteur = 80;
+//ADD CONSTRUCTOR
+	GrandRectangle(int position_x, int position_y, ALLEGRO_COLOR couleur)
+	        : Rectangle(position_x, position_y, couleur) //Cal rectangle constructor
+	    {
+	    }
+};
+
+class MoyenRectangle : public Rectangle
+{
+	public:
+		int largeur = 100;
+		int hauteur = 80;
+//ADD CONSTRUCTOR
+	MoyenRectangle(int position_x, int position_y, ALLEGRO_COLOR couleur)
+	        : Rectangle(position_x, position_y, couleur) //Cal rectangle constructor
+	    {
+	    }
+};
+
+
+//Declaration des fonctions
+void rungame(ALLEGRO_DISPLAY * display, ALLEGRO_EVENT_QUEUE * event_queue);
+void runmenu(ALLEGRO_DISPLAY * display, ALLEGRO_EVENT_QUEUE * event_queue);
 void is_mouse_in_tour(int x, int y, bool *isintour1, bool *isintour2, bool *isintour3);
+void is_mouse_in_button(int x, int y, bool *isinplay, bool *isinIAplay, bool *isineasy, bool *isinmedium, bool *isinhard, GrandRectangle rect_play, GrandRectangle rect_IA_play, MoyenRectangle rect_easy, MoyenRectangle rect_medium, MoyenRectangle rect_hard);
 void display_tours();
+void display_buttons(GrandRectangle rect_play, GrandRectangle rect_IA_play, MoyenRectangle rect_easy, MoyenRectangle rect_medium, MoyenRectangle rect_hard);
+
+
+
+
+
+
+
+
+
+
+
