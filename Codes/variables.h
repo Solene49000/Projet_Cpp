2a9899613b7b2807d2298b5c9162a37d54459e2b
Solene49000 <103738219+Solene49000@users.noreply.github.com>
2022-12-04 11:33:48 +0100

    Update variables.h

--- Codes/variables.h
+++ Codes/variables.h
@@ -1,3 +1,7 @@
 //VARIABLES GOBALES A METTRE DANS LE FICHIER ADAPTE
-#define scrx 640
-#define scry 480
+#define SCRX 640
+#define SCRY 480
+#define BLUE al_map_rgb(10,10,150)
+#define LIGHT_BLUE al_map_rgb(10,10,255)
+#define WHITE al_map_rgb(255,255,255)
+#define BLACK al_map_rgb(0,0,0)
