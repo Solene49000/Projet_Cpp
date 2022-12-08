3606d61bf0cab89157cc9eec762fcab094f8f97f
ambrinou <90826762+ambrinou@users.noreply.github.com>
2022-12-08 11:28:08 +0100

    Delete Codes directory

--- Codes/variables.h
+++
@@ -1,7 +0,0 @@
-//VARIABLES GOBALES A METTRE DANS LE FICHIER ADAPTE
-#define SCRX 640
-#define SCRY 480
-#define BLUE al_map_rgb(10,10,150)
-#define LIGHT_BLUE al_map_rgb(10,10,255)
-#define WHITE al_map_rgb(255,255,255)
-#define BLACK al_map_rgb(0,0,0)
