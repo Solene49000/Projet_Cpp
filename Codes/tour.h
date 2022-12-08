1f0823f09a7ddc3dc2c8a375a215d6b11833a471
ambrinou <90826762+ambrinou@users.noreply.github.com>
2022-12-08 09:41:09 +0100

    Add files via upload

---
+++ Codes/tour.h
@@ -0,0 +1,9 @@
+// PROJET : TOURS DE HANOI - - A.ALLOUIS & S.GUILLAUME
+
+// VARIABLES GLOBALES
+int N_tour = 3;
+int N_etage = 4;
+int hauteur_etage = 20;
+int Largeur_tour = 80;
+int Hauteur_tour = N_etage * hauteur_etage;
+int Epaisseur_tour = 4;
