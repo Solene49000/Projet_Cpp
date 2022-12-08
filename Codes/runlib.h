ac77816962b83339ccf58f6aa7ecbaa381925c6b
Solene49000 <103738219+Solene49000@users.noreply.github.com>
2022-12-08 09:45:47 +0100

    Update runlib.h

--- Codes/runlib.h
+++ Codes/runlib.h
@@ -4,6 +4,7 @@
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_color.h>
 #include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
 #include "variables.h"
 
 //Structures
@@ -47,20 +48,13 @@ class MoyenRectangle : public Rectangle
 
 
 //Declaration des fonctions
-void rungame(ALLEGRO_DISPLAY * display, ALLEGRO_EVENT_QUEUE * event_queue);
+void rungame(ALLEGRO_DISPLAY * display, ALLEGRO_EVENT_QUEUE * event_queue, int * difficulty);
 void runmenu(ALLEGRO_DISPLAY * display, ALLEGRO_EVENT_QUEUE * event_queue);
 void is_mouse_in_tour(int x, int y, bool *isintour1, bool *isintour2, bool *isintour3);
 void is_mouse_in_button(int x, int y, bool *isinplay, bool *isinIAplay, bool *isineasy, bool *isinmedium, bool *isinhard, GrandRectangle rect_play, GrandRectangle rect_IA_play, MoyenRectangle rect_easy, MoyenRectangle rect_medium, MoyenRectangle rect_hard);
 void display_tours();
-void display_buttons(GrandRectangle rect_play, GrandRectangle rect_IA_play, MoyenRectangle rect_easy, MoyenRectangle rect_medium, MoyenRectangle rect_hard, bool * isinplay, bool * isinIAplay, bool * isineasy, bool * isinmedium, bool * isinhard, ALLEGRO_FONT * font, ALLEGRO_FONT * medium_font);
-
-
-
-
-
-
-
-
+void display_buttons(GrandRectangle rect_play, GrandRectangle rect_IA_play, MoyenRectangle rect_easy, MoyenRectangle rect_medium, MoyenRectangle rect_hard, bool * isinplay, bool * isinIAplay, bool * isineasy, bool * isinmedium, bool * isinhard, ALLEGRO_FONT * font, ALLEGRO_FONT * medium_font, int * difficulty);
+void difficulties(int * difficulty, bool* isineasy, bool* isinmedium, bool * isinhard);
 
 
 
